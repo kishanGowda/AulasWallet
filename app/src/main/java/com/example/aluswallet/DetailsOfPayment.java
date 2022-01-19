7397a5ea69eae538f4c6949d759443f0df706415
kishuGowda <kishu.qw@gmail.com>
2022-01-19 15:48:17 +0530

    changes on upi

--- app/src/main/java/com/example/aluswallet/DetailsOfPayment.java
+++ app/src/main/java/com/example/aluswallet/DetailsOfPayment.java
@@ -3,10 +3,15 @@ package com.example.aluswallet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,11 +25,16 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 
+import java.util.ArrayList;
+
 public class DetailsOfPayment extends AppCompatActivity {
     Button button;
     Dialog mdialog;
     ImageView back;
     EditText c1;
+    TextView amount;
+    Button pay;
+    final int UPI_PAYMNT = 0;
 
 
     private TextView upi, other;
@@ -35,14 +45,15 @@ public class DetailsOfPayment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.details_of_payment);
+        amount = findViewById(R.id.total_amount_rs_tv);
 
 
-        mdialog=new Dialog(DetailsOfPayment.this);
+        mdialog = new Dialog(DetailsOfPayment.this);
         mdialog.requestWindowFeature(Window.FEATURE_LEFT_ICON);
 
         mdialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         mdialog.setContentView(R.layout.email);
-        Button next=mdialog.findViewById(R.id.next);
+        Button next = mdialog.findViewById(R.id.next);
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,21 +75,14 @@ public class DetailsOfPayment extends AppCompatActivity {
                     upi.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            Dialog dialog = new Dialog(DetailsOfPayment.this);
-                            dialog.requestWindowFeature(Window.FEATURE_LEFT_ICON);
-                            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                            dialog.setCancelable(false);
-                            dialog.setContentView(R.layout.payment_succesfull_dialog);
-                            Button dialogButton = (Button) dialog.findViewById(R.id.back_to_home_button);
-                            dialogButton.setOnClickListener(new View.OnClickListener() {
-                                @Override
-                                public void onClick(View v) {
-                                    
-                                    Intent intent = new Intent(getApplicationContext(), AlusAdmissionFee.class);
-                                    startActivity(intent);
-                                }
-                            });
-                            dialog.show();
+
+                            String amt = "1";
+                            String upitxt = "9901153220@paytm";
+                            String upiname = "kishan";
+                            String upinote = "kishan";
+                            payUsingupi(amt, upitxt, upiname, upinote);
+
+
                         }
                     });
                     other.setOnClickListener(new View.OnClickListener() {
@@ -94,7 +98,7 @@ public class DetailsOfPayment extends AppCompatActivity {
             }
         });
 
-        ImageView cancel=(ImageView)mdialog.findViewById(R.id.cancel_imageView_email);
+        ImageView cancel = (ImageView) mdialog.findViewById(R.id.cancel_imageView_email);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -110,7 +114,146 @@ public class DetailsOfPayment extends AppCompatActivity {
                 mdialog.show();
             }
         });
-
-
     }
-}
+
+        private void payUsingupi(String amt, String upitxt, String upiname, String upinote) {
+            Uri uri = Uri.parse("upi://pay").buildUpon().appendQueryParameter("pa", upitxt)
+                    .appendQueryParameter("pn", upiname)
+                    .appendQueryParameter("tn", upinote)
+                    .appendQueryParameter("am", amt)
+                    .appendQueryParameter("mc", "")
+                    //.appendQueryParameter("tid", "02125412")
+                    .appendQueryParameter("tr", "25584584")
+                    .appendQueryParameter("cu", "INR").build();
+            Intent upi_payment = new Intent(Intent.ACTION_VIEW);
+            upi_payment.setData(uri);
+            Intent chooser = Intent.createChooser(upi_payment, "pay with");
+            if (null != chooser.resolveActivity(getPackageManager())) {
+                startActivityForResult(chooser, UPI_PAYMNT);
+
+            } else {
+                Toast.makeText(DetailsOfPayment.this, "no upi app found", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        private void initialiemethod() {
+            upi = (TextView) findViewById(R.id.upi_tv);
+            amount = (TextView) findViewById(R.id.total_amount_rs_tv);
+        }
+
+
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+            super.onActivityResult(requestCode, resultCode, data);
+            switch (requestCode) {
+                case UPI_PAYMNT:
+                    if ((RESULT_OK == resultCode || (requestCode == 11))) {
+                        if (data != null) {
+                            String txt = data.getStringExtra("response");
+                            Log.d("UPI", "onActivityResults:" + txt);
+                            ArrayList<String> dataLst = new ArrayList<>();
+                            dataLst.add("Nothing");
+                            upipaymentdataoperation(dataLst);
+
+                        } else {
+                            Log.d("UPI", "onActivityResults:" + "Return data is null");
+                            ArrayList<String> dataLst = new ArrayList<>();
+                            dataLst.add("Nothing");
+                            upipaymentdataoperation(dataLst);
+                        }
+                    } else {
+                        Log.d("UPI", "onActivityResults:" + "Return data is null");
+                        ArrayList<String> dataLst = new ArrayList<>();
+                        dataLst.add("Nothing");
+                        upipaymentdataoperation(dataLst);
+                    }
+
+                    break;
+
+            }
+        }
+
+        private void upipaymentdataoperation(ArrayList<String> data) {
+            if (isConnectionAvaliable(DetailsOfPayment.this)) {
+                String str = data.get(0);
+                Log.d("UPIPAY", "upipaymentoperation:" + str);
+                String paymentCancel = "";
+                if (str == null) str = "discard";
+                String status = "";
+                String approvalref = "";
+                String response[] = str.split("&");
+                for (int i = 0; i < response.length; i++) {
+                    String equalStr[] = response[i].split("=");
+                    if (equalStr.length >= 2) {
+                        if (equalStr[0].toLowerCase().equals("Status".toLowerCase())) {
+                            status = equalStr[1].toLowerCase();
+                        } else if (equalStr[0].toLowerCase().equals("approval Ref".toLowerCase()) ||
+                                equalStr[0].toLowerCase().equals("txnRef".toLowerCase())) {
+                            approvalref = equalStr[1];
+                        }
+
+                    } else {
+                        paymentCancel = "payment cancel by user";
+                    }
+                }
+                if (status.equals("success")) {
+                    Toast.makeText(getApplicationContext(), "transaction sucess", Toast.LENGTH_SHORT).show();
+                    Log.d("UPI", "responsestr:" + approvalref);
+
+                } else if ("payment cancel by user".equals(paymentCancel))
+
+                {
+                    mBottomSheetDialog.dismiss();
+                    Dialog canceldialog = new Dialog(DetailsOfPayment.this, androidx.appcompat.R.style.Theme_AppCompat_Dialog);
+                    canceldialog.requestWindowFeature(Window.FEATURE_LEFT_ICON);
+                    canceldialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    canceldialog.setCancelable(false);
+                    canceldialog.setContentView(R.layout.payment_cancel);
+                    Button tryagain=(Button)canceldialog.findViewById(R.id.tryagain_btn);
+                    tryagain.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            canceldialog.dismiss();
+                        }
+                    });
+
+                    canceldialog.show();
+                    Toast.makeText(DetailsOfPayment.this, "payment cancel by user", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    mBottomSheetDialog.dismiss();
+                    Dialog canceldialog = new Dialog(DetailsOfPayment.this, androidx.appcompat.R.style.Theme_AppCompat_Dialog);
+                    canceldialog.requestWindowFeature(Window.FEATURE_LEFT_ICON);
+                    canceldialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                    canceldialog.setCancelable(false);
+                    canceldialog.setContentView(R.layout.transaction_failed);
+                    Button tryagain=(Button)canceldialog.findViewById(R.id.tryagain_btn);
+                    tryagain.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            canceldialog.dismiss();
+                        }
+                    });
+
+                    canceldialog.show();
+                    Toast.makeText(DetailsOfPayment.this, "transaction failed", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(DetailsOfPayment.this, "internet connection is not avialable", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        private boolean isConnectionAvaliable(Context context){
+            ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (connectivityManager != null) {
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected() && networkInfo.isConnectedOrConnecting()
+                        && networkInfo.isAvailable()) {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+    }
